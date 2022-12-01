66bd7b40b2c497ba4dde8d7c8c1a33e33283a58c
Andrew Greenman <a.j.greenman@gmail.com>
2022-12-01 11:29:08 -0500

    initial commit

---
+++ aoc22/aoc22App.swift
@@ -0,0 +1,17 @@
+//
+//  aoc22App.swift
+//  aoc22
+//
+//  Created by Andrew Greenman on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct aoc22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
