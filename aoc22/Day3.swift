f672217041e70035e6a1b62400f492e95d997a2c
Andrew Greenman <a.j.greenman@gmail.com>
2022-12-03 17:10:49 -0500

    part 2 complete

--- aoc22/Day3.swift
+++ aoc22/Day3.swift
@@ -13,25 +13,62 @@ class Day3 {
             return
         }
         
+        print(_part1(input))
+        print(_part2(input))
+    }
+    
+    static func _part1(_ input: String) -> Int {
         var total = 0
         
         input.enumerateLines { line, _ in
-            let firstChunk = Set(String(line[..<line.index(line.startIndex, offsetBy: line.count / 2)]))
-            let secondChunk = Set(String(line[line.index(line.endIndex, offsetBy: -line.count / 2)...]))
+            let firstChunk = String(line[..<line.index(line.startIndex, offsetBy: line.count / 2)])
+            let secondChunk = String(line[line.index(line.endIndex, offsetBy: -line.count / 2)...])
             
-            guard let commonValue = firstChunk.intersection(secondChunk).first else {
+            guard let commonValue = Set(firstChunk).intersection(Set(secondChunk)).first else {
                 return
             }
             
-            var characterValue: Int
-            if(commonValue.isUppercase) {
-                characterValue = Int(commonValue.asciiValue!) - 38
-            } else {
-                characterValue = Int(commonValue.asciiValue!) - 96
-            }
-            total += characterValue
+            total += _getCharacterValue(commonValue)
         }
         
-        print(total)
+        return total
+    }
+    
+    static func _part2(_ input: String) -> Int {
+        var total = 0
+        
+        var group = [String]()
+        
+        input.enumerateLines { line, _ in
+            group.append(line)
+            
+            if(group.count == 3) {
+                let first = Set(group[0])
+                let second = Set(group[1])
+                let third = Set(group[2])
+                
+                guard let commonValue = first.intersection(second).intersection(third).first else {
+                    return
+                }
+                
+                total += _getCharacterValue(commonValue)
+                
+                group.removeAll()
+            }
+        }
+        
+        return total
+    }
+    
+    static func _getCharacterValue(_ s: String.Element) -> Int {
+        var characterValue: Int
+        
+        if(s.isUppercase) {
+            characterValue = Int(s.asciiValue!) - 38
+        } else {
+            characterValue = Int(s.asciiValue!) - 96
+        }
+        
+        return characterValue
     }
 }
